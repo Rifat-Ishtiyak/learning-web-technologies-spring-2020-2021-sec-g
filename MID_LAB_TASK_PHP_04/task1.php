450125b214685e31004d6abff32fdf4f81ec12cd
Rifat-Ishtiyak <rifatishtiyak@gmail.com>
2021-02-15 16:57:53 +0600

    task 1 added

---
+++ MID_LAB_TASK_PHP_04/task1.php
@@ -0,0 +1,11 @@
+<?php 
+    $length=readline('Enter length: ');
+    $width=readline('Enter width: ');
+    
+    $area = ($length*$width);
+    $perimeter = 2*($length+$width);
+
+    echo('Area of Triangle: '.$area."\n");
+    
+    echo('Perimeter of Triangle: '.$perimeter."\n")
+?>
