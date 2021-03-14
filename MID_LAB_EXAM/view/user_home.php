2b15c46022300c031e5864b8fc39f84c32dedc65
Rifat-Ishtiyak <rifatishtiyak@gmail.com>
2021-03-14 11:00:13 +0600

    2nd commit at 10:58 AM

---
+++ MID_LAB_EXAM/view/user_home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<center>
+	<h1>Welcome !</h1>
+	<a href="profile.html">Profile</a>
+	<br/>
+	<a href="change_password.html">Change Password</a>
+	<br/>
+	<a href="login.html">Logout</a>
+</center>
+</body>
+</html>
