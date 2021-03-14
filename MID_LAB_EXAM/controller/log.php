5cedd7ecaf2c6d02d736a2f8020ce9710e7eda5d
Rifat-Ishtiyak <rifatishtiyak@gmail.com>
2021-03-14 11:16:26 +0600

    mmit at 10:58 AM

--- MID_LAB_EXAM/controller/log.php
+++ MID_LAB_EXAM/controller/log.php
@@ -1,5 +1,6 @@
 <?php 
     session_start();
+
     if(isset($_POST['submit'])){
         $inp = file_get_contents('../model/user.json');
 		$temp = json_decode($inp, true);
@@ -12,7 +13,7 @@
 			foreach ($temp as $key1 => $value1) {
 				if ($temp[$key1]["id"]==$_POST['id'] and $temp[$key1]["pass"]==$_POST['pass']) {
 					$f=1;
-                    $_SESSION['nam']=$temp[$key1]["name"];
+                    $name=$temp[$key1]["name"];
                     $type=$temp[$key1]["type"];
 				}
 
@@ -20,8 +21,14 @@
 			if($f==1){
 				$_SESSION['flag'] = true;
 				$name=$_POST['username'];
-				
-				header('location: ../view/home.php');
+				if ($type=='User') {
+					$_SESSION['nam']=$name;
+					header('location: ../view/user_home.php');
+				}
+				elseif ($type=='Admin') {
+					$_SESSION['nam']=$name;
+					header('location: ../view/admin_home.php');
+				}
 			}
 			else{
 				echo"invaild user";
--- MID_LAB_EXAM/view/user_home.php
+++ MID_LAB_EXAM/view/user_home.php
@@ -7,13 +7,13 @@
     <title>Document</title>
 </head>
 <body>
-<center>
-	<h1>Welcome !</h1>
-	<a href="profile.html">Profile</a>
-	<br/>
-	<a href="change_password.html">Change Password</a>
-	<br/>
-	<a href="login.html">Logout</a>
-</center>
+	<center>
+		<h1>Welcome !</h1>
+		<a href="profile.html">Profile</a>
+		<br/>
+		<a href="change_password.html">Change Password</a>
+		<br/>
+		<a href="../controller/logout.php">Logout</a>
+	</center>
 </body>
 </html>
